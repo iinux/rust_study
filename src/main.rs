42bec8d618d59186f9ca25481e58e5848f62a025
iinux <iinux@qq.com>
2019-05-10 16:03:00 +0800

    guess the number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+    println!("{} {} {}", five_hundred, six_point_four, one);
+
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{} {}", months[0], a[1]);
+}
+
+fn main1() {
+    let mut r#fn = 1u8;
+    println!("{}", r#fn);
+    r#fn += 255;
+    const MAX_POINTS: u32 = 100_000;
+    println!("{} {}", r#fn, MAX_POINTS);
+
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        /*
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
+            */
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            // Err(_) => continue,
+            Err(e) => {
+                println!("ERROR: {}", e);
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
